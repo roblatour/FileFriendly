4a7bbf8f9aa074ec0db5c6e9140c556edcb65214
Rob Latour <5200730+roblatour@users.noreply.github.com>
2025-12-21 17:27:04 -0500

    Update PickAFolder.xaml.vb

--- filefriendly/PickAFolder.xaml.vb
+++ filefriendly/PickAFolder.xaml.vb
@@ -519,12 +519,13 @@
         Else
 
             If QuickFilterWord.Length = 0 Then
-                'first character must be a letter or number
+                'first character must be a letter or number or "!"
                 Dim ch As Char = IncomingText(0)
-                If (ch < "A"c OrElse ch > "Z"c) AndAlso (ch < "a"c OrElse ch > "z"c) AndAlso (ch < "0"c OrElse ch > "9"c) Then
+                If (ch < "A"c OrElse ch > "Z"c) AndAlso (ch < "a"c OrElse ch > "z"c) AndAlso (ch < "0"c OrElse ch > "9"c) AndAlso (ch <> "!"c) Then
                     'invalid first character
                     Exit Sub
                 End If
+
             End If
 
             QuickFilterWord &= IncomingText
